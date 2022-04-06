226de13ebab82426bee531cfc94df4b7e2179584
Your Name <hakanor99@gmail.com>
2022-04-06 11:03:33 +0300

    Listing feature added

--- lib/Screens/Listing Screens/test_detail.dart
+++ lib/Screens/Listing Screens/test_detail.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
+import 'package:ride_sharing_app/Screens/MyHomePage.dart';
 
 class DetailPage extends StatefulWidget {
 
@@ -21,13 +22,13 @@ class _DetailPageState extends State<DetailPage> {
     _dateinput.text="";
     super.initState();
   }
-
   TextEditingController _timeinput = new TextEditingController();
   TextEditingController _dateinput = new TextEditingController();
   TextEditingController _seatcount = new TextEditingController();
   TextEditingController _carbrand = new TextEditingController();
   TextEditingController _carmodel = new TextEditingController();
   TextEditingController _platenumber = new TextEditingController();
+  TextEditingController _price = new TextEditingController();
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -63,214 +64,257 @@ class _DetailPageState extends State<DetailPage> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(title: Text("İlan Detaylarını Gir"),),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 40,
-                  width: 350,
-                  child: TextField(
-                    controller: _timeinput,
-                    readOnly: true,
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: (){
-                          setState(() {
-                            _timeinput.text="";
-                          });
-                        },
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40,
+                    width: 350,
+                    child: TextFormField(
+                      controller: _timeinput,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: (){
+                            setState(() {
+                              _timeinput.text="";
+                            });
+                          },
+                        ),
+                        labelText: "Çıkış Saati Seçin",
+                        labelStyle: TextStyle(fontSize: 16),
+                        border: OutlineInputBorder(),
                       ),
-                      labelText: "Çıkış Saati Seçin",
-                      labelStyle: TextStyle(fontSize: 16),
-                      border: OutlineInputBorder(),
-                    ),
-                    onTap: () async {
-                      TimeOfDay? pickedTime=await showTimePicker(
-                        helpText: "Çıkış saati Seçin",
-                          cancelText: "İptal",
-                          confirmText: "Onayla",
-                          hourLabelText: "Saat",
-                          minuteLabelText: "Dakika",
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                          initialEntryMode: TimePickerEntryMode.dial,
-                          builder: (context,child){
-                            return MediaQuery(
-                                data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-                                child: child ?? Container(),)
+                      onTap: () async {
+                        TimeOfDay? pickedTime=await showTimePicker(
+                          helpText: "Çıkış saati Seçin",
+                            cancelText: "İptal",
+                            confirmText: "Onayla",
+                            hourLabelText: "Saat",
+                            minuteLabelText: "Dakika",
+                            context: context,
+                            initialTime: TimeOfDay.now(),
+                            initialEntryMode: TimePickerEntryMode.dial,
+                            builder: (context,child){
+                              return MediaQuery(
+                                  data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+                                  child: child ?? Container(),)
+                            }
+                        );
+
+                        if(pickedTime != null ){
+
+                          String _hour = pickedTime.hour.toString();
+                          String _minute = pickedTime.minute.toString();
+                          if(pickedTime.hour<10){
+                            _hour="0"+_hour;
                           }
-                      );
+                          if(pickedTime.minute<10){
+                            _minute="0"+_minute;
+                          }
 
-                      if(pickedTime != null ){
+                          String formattedTime = _hour + ':' + _minute;
+                          print(formattedTime);
 
-                        String _hour = pickedTime.hour.toString();
-                        String _minute = pickedTime.minute.toString();
-                        if(pickedTime.hour<10){
-                          _hour="0"+_hour;
-                        }
-                        if(pickedTime.minute<10){
-                          _minute="0"+_minute;
-                        }
-
-                        String formattedTime = _hour + ':' + _minute;
-                        print(formattedTime);
-
-                        setState(() {
-                          _timeinput.text=formattedTime;
-                        });
-                      }else{
-                        print("Saat Seçilmedi");
-                      }
-                    },
-                  ),
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 40,
-                  width: 350,
-                  child: TextField(
-                    controller: _dateinput,
-                    readOnly: true,
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.clear),
-                        onPressed: (){
                           setState(() {
-                            _dateinput.text="";
+                            _timeinput.text=formattedTime;
                           });
-                        },
+                        }else{
+                          print("Saat Seçilmedi");
+                        }
+                      },
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40,
+                    width: 350,
+                    child: TextField(
+                      controller: _dateinput,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: (){
+                            setState(() {
+                              _dateinput.text="";
+                            });
+                          },
+                        ),
+                        labelText: "Çıkış Tarihi Seçin",
+                        labelStyle: TextStyle(fontSize: 16),
+                        border: OutlineInputBorder(),
                       ),
-                      labelText: "Çıkış Tarihi Seçin",
-                      labelStyle: TextStyle(fontSize: 16),
-                      border: OutlineInputBorder(),
-                    ),
-                    onTap: () async {
-                      DateTime? pickedDate=await showDatePicker(
-                          locale: const Locale("tr", "TR"),
-                          helpText: "Yolculuk için tarih seçiniz.",
-                          cancelText: "İptal",
-                          confirmText: "Onayla",
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2101),
-                      );
-                      if(pickedDate != null ){
-                        print(pickedDate);
-                        String formattedDate = DateFormat('dd.MM.yyyy').format(pickedDate);
-                        print(formattedDate);
-                        setState(() {
-                          _dateinput.text=formattedDate;
-                        });
-                      }else{
-                        print("Tarih Seçilmedi");
-                      }
-                    },
-                  ),
-                ),
-              ),
-
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 40,
-                  width: 350,
-                  child: TextFormField(
-                    controller: _seatcount,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: "Kaç Kişilik yeriniz var?",
-                      labelStyle: TextStyle(fontSize: 16,),
-                      border: OutlineInputBorder(),
+                      onTap: () async {
+                        DateTime? pickedDate=await showDatePicker(
+                            locale: const Locale("tr", "TR"),
+                            helpText: "Yolculuk için tarih seçiniz.",
+                            cancelText: "İptal",
+                            confirmText: "Onayla",
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2101),
+                        );
+                        if(pickedDate != null ){
+                          print(pickedDate);
+                          String formattedDate = DateFormat('dd.MM.yyyy').format(pickedDate);
+                          print(formattedDate);
+                          setState(() {
+                            _dateinput.text=formattedDate;
+                          });
+                        }else{
+                          print("Tarih Seçilmedi");
+                        }
+                      },
                     ),
                   ),
                 ),
-              ),
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 40,
-                  width: 350,
-                  child: TextFormField(
-                    controller:_carbrand,
-                    decoration: InputDecoration(
-                      labelText: "Araç Markası Girin",
-                      labelStyle: TextStyle(fontSize: 16),
-                      border: OutlineInputBorder(),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40,
+                    width: 350,
+                    child: TextFormField(
+                      controller: _seatcount,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Kaç Kişilik yeriniz var?",
+                        labelStyle: TextStyle(fontSize: 16,),
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 40,
-                  width: 350,
-                  child: TextFormField(
-                    controller: _carmodel,
-                    decoration: InputDecoration(
-                      labelText: "Araç Modeli Girin",
-                      labelStyle: TextStyle(fontSize: 16),
-                      border: OutlineInputBorder(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40,
+                    width: 350,
+                    child: TextFormField(
+                      controller:_carbrand,
+                      decoration: InputDecoration(
+                        labelText: "Araç Markası Girin",
+                        labelStyle: TextStyle(fontSize: 16),
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 40,
-                  width: 350,
-                  child: TextFormField(
-                    controller: _platenumber,
-                    decoration: InputDecoration(
-                      labelText: "Araç Palakası Giriniz",
-                      labelStyle: TextStyle(fontSize: 16),
-                      border: OutlineInputBorder(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40,
+                    width: 350,
+                    child: TextFormField(
+                      controller: _carmodel,
+                      decoration: InputDecoration(
+                        labelText: "Araç Modeli Girin",
+                        labelStyle: TextStyle(fontSize: 16),
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 8),
-                  child: ElevatedButton(
-                    child: Text("İlan Ver"),
-                    onPressed: (){
-                      createListing(
-                          widget.start_location,
-                          widget.end_location,
-                          _dateinput.text,
-                          _timeinput.text,
-                          int.parse(_seatcount.text),
-                          _carbrand.text,
-                          _carmodel.text,
-                          _platenumber.text,
-                      );
-                    },
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40,
+                    width: 350,
+                    child: TextFormField(
+                      controller: _platenumber,
+                      decoration: InputDecoration(
+                        labelText: "Araç Palakası Giriniz",
+                        labelStyle: TextStyle(fontSize: 16),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
 
+                Padding(
+                  padding: const EdgeInsets.only(left:0,top: 0),
+                  child: Row(
+                    children: [
+
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 40,
+                          width: 125,
+                          child: TextFormField(
+                            controller: _price,
+                            decoration: InputDecoration(
+                              labelText: "Ücret Giriniz",
+                              labelStyle: TextStyle(fontSize: 16),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        height: 40,
+                        width: 150,
+                        child: Center(child: Text("",style: TextStyle(color: Colors.black87),)),
+                      ),
+
+                    ],
+                  )
+                ),
+
+
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: ElevatedButton(
+                      child: Text("İlan Ver"),
+                      onPressed: (){
+                            if (_timeinput.text==null || _dateinput.text==null || _seatcount.text==null
+                                || _carbrand.text==null || _carmodel.text==null || _platenumber.text==null
+                            ) {
+                              Fluttertoast.showToast(msg: "Tüm alanların doldurulması gerekmektedir.");
+                            }
+                            else{
+                              createListing(
+                                widget.start_location,
+                                widget.end_location,
+                                _dateinput.text,
+                                _timeinput.text,
+                                int.parse(_seatcount.text),
+                                _carbrand.text,
+                                _carmodel.text,
+                                _platenumber.text,
+                              );
+                              Navigator.popUntil(context, (route) =>route.isFirst);
+                            }
+
+                      },
+                    ),
+                  ),
+                ),
+              ],
+
+            ),
           ),
         ),
       ),
--- lib/Screens/Listing Screens/test_screen.dart
+++ lib/Screens/Listing Screens/test_screen.dart
@@ -1,6 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_api_headers/google_api_headers.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_webservice/places.dart';
@@ -17,7 +20,7 @@ class _test_screenState extends State<test_screen> {
   String? googleApikey=dotenv.env['GOOGLE_API_KEY'];
   GoogleMapController? mapController; //controller for Google map
   CameraPosition? cameraPosition;
-  LatLng startLocation = LatLng(37.87121931889777, 32.505299407385266); // init map location (konya mevlana müzesi)
+  LatLng startLocation = LatLng(37.87121931889777, 32.505299407385266);// init map location (konya mevlana müzesi)
 
   String location = "Başlangıç Noktası";
   String location2= "Bitiş Noktası";
@@ -25,12 +28,73 @@ class _test_screenState extends State<test_screen> {
   var placeid1; // placeid for textfield1
   var placeid2; // placeid for textfield2
 
+  late LatLng place1; // textfield1 için place LatLng
+  late LatLng place2; // textfield2 için place LatLng
+
+  // Google autocomplete textfield
   String start_location="";
   String end_location="";
 
+  // Konya kulesite önü lat lang
+  static const nycLat=37.88860206217567;
+  static const nycLng=32.49354872609671;
+
+
+  // verilen keyword ile belirli konumda arama yapılmasını sağlıyor
+  Future <List<String>> searchNearby(String keyword) async{
+    var dio= Dio();
+    var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+    var parameters={
+      'key':googleApikey,
+      'location':'$nycLat,$nycLng',
+      'radius':'800',
+      'keyword':keyword,
+    };
+    var response = await dio.get(url,queryParameters: parameters);
+    return response.data['results'].map<String>((result)=>result['name'].toString()).toList();
+  }
+
+  // keyword olmadan konumda arama yapılmasını sağlıyor
+  Future <List<String>> searchNearbyWithout() async{
+    var dio= Dio();
+    var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+    var parameters={
+      'key':googleApikey,
+      'location':'$nycLat,$nycLng',
+      'radius':'800',
+    };
+    var response = await dio.get(url,queryParameters: parameters);
+    return response.data['results'].map<String>((result)=>result['name'].toString()).toList();
+  }
+
+  // konumu dışarıdan girerek arama yapılmasını sağlıyor
+  Future <List<String>> searchNearbyService(var lat, var lng) async{
+    var dio= Dio();
+    var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+    var parameters={
+      'key':googleApikey,
+      'location':'$lat,$lng',
+      'radius':'800',
+    };
+    var response = await dio.get(url,queryParameters: parameters);
+    return response.data['results'].map<String>((result)=>result['name'].toString()).toList();
+  }
+
+  //
 
   // ------------------- marker işlemleri ------------------------------------//
   Set<Marker> markers = Set<Marker>(); // Marker list
+  Set<Polyline> _polylines = Set<Polyline>();
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints= PolylinePoints();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    polylinePoints = PolylinePoints();
+    super.initState();
+  }
+
 
   // setmarker
   void setMarker(LatLng point){
@@ -40,6 +104,29 @@ class _test_screenState extends State<test_screen> {
             markerId: MarkerId('marker'),
             position:point,
           )
+      );
+    });
+  }
+
+  void setPolylines() async{
+    PolylineResult result=await polylinePoints.getRouteBetweenCoordinates(
+        googleApikey!,
+        PointLatLng(place1.latitude, place1.longitude),
+        PointLatLng(place2.latitude, place2.longitude),
+    );
+    print(result.status);
+    print(result.errorMessage);
+    if (result.status=='OK'){
+      result.points.forEach((PointLatLng point) {polylineCoordinates.add(LatLng(point.latitude, point.longitude)); });
+    }
+    setState(() {
+      _polylines.add(
+        Polyline(
+          width: 10,
+          polylineId: PolylineId('polyLine'),
+          color: Color(0xFF08A5CB),
+          points: polylineCoordinates,
+        )
       );
     });
   }
@@ -57,6 +144,7 @@ class _test_screenState extends State<test_screen> {
               GoogleMap( //Map widget from google_maps_flutter package
                 zoomGesturesEnabled: true,
                 markers: markers,
+                polylines: _polylines,
                 initialCameraPosition: CameraPosition( //innital position in map
                   target: startLocation, //initial position
                   zoom: 14.0, //initial zoom level
@@ -107,6 +195,7 @@ class _test_screenState extends State<test_screen> {
                           final lat = geometry.location.lat;
                           final lang = geometry.location.lng;
                           var newlatlang = LatLng(lat, lang);
+                          place1=newlatlang;
 
                           //setMarker(newlatlang);
 
@@ -121,7 +210,6 @@ class _test_screenState extends State<test_screen> {
                             markers.add(startMarker);
                             placeid1=placeid;
                           });
-
 
                           //move map camera to selected place with animation
                           mapController?.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: newlatlang, zoom: 17)));
@@ -182,6 +270,7 @@ class _test_screenState extends State<test_screen> {
                           final lat = geometry.location.lat;
                           final lang = geometry.location.lng;
                           var newlatlang = LatLng(lat, lang);
+                          place2=newlatlang;
 
                           //setMarker(newlatlang);
 
@@ -225,24 +314,48 @@ class _test_screenState extends State<test_screen> {
                   child: ElevatedButton(
                       child: Text("Yolculuk Bilgilerini Gir"),
                       onPressed: (){
-                        /* TEST İÇİN
                         if(placeid1==null && placeid2==null){
                           Fluttertoast.showToast(msg: "Başlangıç ve Bitiş noktası belirtilmek zorunda!");
                         }
                         else if(placeid1==null){Fluttertoast.showToast(msg: "Başlangıç noktası belirtilmek zorunda!");}
                         else if(placeid2==null){Fluttertoast.showToast(msg: "Bitiş noktası belirtilmek zorunda!");}
                         else{
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage()));
-                        }*/
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(start_location:location , end_location: location2,)));
+                        }
 
-                        Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                        /*Navigator.push(context, MaterialPageRoute(builder: (context) =>
                             DetailPage(
                               start_location:location , end_location: location2,
-                            )));
+                            )));*/
                       },
                   )
               ),
 
+              /*Positioned(
+                top: 400,
+                left: 20,
+                child: ElevatedButton(onPressed: () {
+                  setState(()  {
+                    _polylines.clear();
+                    polylineCoordinates.clear();
+                    setPolylines();
+                  });
+                  print("polylineCoord");
+                  print(polylineCoordinates);
+
+                }, child: Text("Test")),
+              ),
+
+              Positioned(
+                top: 500,
+                left: 20,
+                child: ElevatedButton(onPressed: () async {
+                    print(await searchNearby('Mevlana Otopark'));
+                    print(await "Selam");
+                    print(await searchNearbyWithout());
+                }, child: Text("test2")),
+              )
+            */
             ]
         )
     );
